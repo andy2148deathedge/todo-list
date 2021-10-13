f6581e1bbbcf6304551c8e9ff37332892115d998
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-13 18:52:46 +0800

    feat: use method-override to update more Restful than before.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const Todo = require('./models/todo');
 
@@ -22,7 +23,9 @@ db.once('open', () => {
   console.log('mongodb connected');
 })
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   Todo.find()
@@ -64,7 +67,7 @@ app.get('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error));
 })
 
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   const id = req.params.id;
   const { name, isDone } = req.body; // Destructing req object
 
@@ -79,7 +82,7 @@ app.post('/todos/:id/edit', (req, res) => {
     
 })
 
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
   const id = req.params.id;
   return Todo.findById(id)
     .then(todo => todo.remove())
