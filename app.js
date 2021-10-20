7f9014e31b36f1e158b97259eb8d015b70d3b24a
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-20 18:57:10 +0800

    feat: setting express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import npm(現成)套件
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
@@ -14,6 +15,12 @@ const PORT = process.env.PORT || 3000;
 // 各種設置 & middleware
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');  
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true,
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
